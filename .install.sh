c5912c47024f1607cb14a80e7d03e62b53ea1e07
Bryton Lacquement <contact@brytonlacquement.com>
2022-09-11 12:21:22 +0200

    install: add script to set up dotfiles

--- .install.sh
+++ .install.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+if [[ ! -d "$HOME/.dotfiles" ]]; then
+  git clone --bare git@gitlab.com:bminusl/dotfiles.git $HOME/.dotfiles
+  # or git clone --bare https://gitlab.com/bminusl/dotfiles.git $HOME/.dotfiles
+  git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout main -f
+  git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
+fi
+
 sudo apt-get update
 sudo apt-get install -y \
   curl \
