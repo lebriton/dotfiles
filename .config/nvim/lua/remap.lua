14dc3d5bf7e60d625688be19a4aa7f89ab23692e
Bryton Lacquement <hello@brytonlacquement.com>
2023-10-14 16:56:44 +0200

    nvim: map <leader>w to <C-w>

--- .config/nvim/lua/remap.lua
+++ .config/nvim/lua/remap.lua
@@ -4,3 +4,5 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>bn", function() vim.cmd('bn') end)
 vim.keymap.set("n", "<leader>bp", function() vim.cmd('bp') end)
 vim.keymap.set("n", "<leader>bd", function() vim.cmd('bd') end)
+
+vim.keymap.set("n", "<leader>w", '<C-w>')
