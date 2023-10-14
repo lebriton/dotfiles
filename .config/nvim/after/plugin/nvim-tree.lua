33be561c4f2a92dd11df6e181bc31f4df6398f96
Bryton Lacquement <hello@brytonlacquement.com>
2023-10-15 01:36:38 +0200

    nvim: remove the hacky autoclose feature of nvim-tree

--- .config/nvim/after/plugin/nvim-tree.lua
+++ .config/nvim/after/plugin/nvim-tree.lua
@@ -28,25 +28,3 @@ require("nvim-tree").setup({
 
 local api = require "nvim-tree.api"
 vim.keymap.set("n", "<leader>pt", function() api.tree.toggle{focus = false} end)
-
--- https://github.com/nvim-tree/nvim-tree.lua/issues/1005#issuecomment-1183468091
--- nvim-tree is also there in modified buffers so this function filter it out
-local modifiedBufs = function(bufs)
-    local t = 0
-    for k,v in pairs(bufs) do
-        if v.name:match("NvimTree_") == nil then
-            t = t + 1
-        end
-    end
-    return t
-end
-vim.api.nvim_create_autocmd("BufEnter", {
-    nested = true,
-    callback = function()
-        if #vim.api.nvim_list_wins() == 1 and
-        vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil and
-        modifiedBufs(vim.fn.getbufinfo({bufmodified = 1})) == 0 then
-            vim.cmd "quit"
-        end
-    end
-})
