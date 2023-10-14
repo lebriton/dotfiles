596ce92b81d991d9f5d0a7915c2eed0722a41851
Bryton Lacquement <hello@brytonlacquement.com>
2023-10-14 19:44:01 +0200

    nvim: remove '<leader>pv'

--- .config/nvim/lua/remap.lua
+++ .config/nvim/lua/remap.lua
@@ -1,5 +1,4 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) 
 
 vim.keymap.set("n", "<leader>bn", function() vim.cmd('bn') end)
 vim.keymap.set("n", "<leader>bp", function() vim.cmd('bp') end)
