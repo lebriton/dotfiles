8ae0c015c67a30b87ff67e11208aa8aa213084c7
Bryton Lacquement <hello@brytonlacquement.com>
2023-10-14 16:27:58 +0200

    nvim: add buffer keymaps

--- .config/nvim/after/plugin/nvim-tree.lua
+++ .config/nvim/after/plugin/nvim-tree.lua
@@ -6,18 +6,18 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
 -- empty setup using defaults
-require("nvim-tree").setup()
+-- require("nvim-tree").setup()
 
 -- OR setup with some options
 require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  view = {
-    width = 30,
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
+    sort_by = "case_sensitive",
+    view = {
+        width = 30,
+    },
+    renderer = {
+        group_empty = true,
+    },
+    filters = {
+        dotfiles = true,
+    },
 })
--- .config/nvim/lua/remap.lua
+++ .config/nvim/lua/remap.lua
@@ -1,2 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) 
+
+vim.keymap.set("n", "<leader>bn", function() vim.cmd('bn') end)
+vim.keymap.set("n", "<leader>bp", function() vim.cmd('bp') end)
+vim.keymap.set("n", "<leader>bd", function() vim.cmd('bd') end)
