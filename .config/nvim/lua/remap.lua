8fc0916dccf73beefae8e99db4fde3e227e0a94c
Bryton Lacquement <hello@brytonlacquement.com>
2023-10-19 23:23:05 +0200

    nvim: improve the split keymaps

--- .config/nvim/lua/remap.lua
+++ .config/nvim/lua/remap.lua
@@ -5,10 +5,10 @@ vim.keymap.set("n", "<leader>w", '<C-w>')
 vim.keymap.set("n", "x", '"_x')  -- do not copy to a register
 
 -- split keymaps
-vim.keymap.set("n", "<leader>sh", ":belowright vsp<CR>")  -- split window left
-vim.keymap.set("n", "<leader>sj", ":sp<CR>")  -- split window down
-vim.keymap.set("n", "<leader>sk", ":belowright sp<CR>")  -- split window up
-vim.keymap.set("n", "<leader>sl", ":vsp<CR>")  -- split window right
+vim.keymap.set("n", "<leader>sh", ":belowright vsp<CR><C-w>h<CR>")  -- split window left
+vim.keymap.set("n", "<leader>sj", ":sp<CR><C-w>j<CR>")  -- split window down
+vim.keymap.set("n", "<leader>sk", ":belowright sp<CR><C-w>k<CR>")  -- split window up
+vim.keymap.set("n", "<leader>sl", ":vsp<CR><C-w>l<CR>")  -- split window right
 vim.keymap.set("n", "<leader>se", "<C-w>=")  -- make split windows equal width
 vim.keymap.set("n", "<leader>sx", ":close<CR>")  -- close current split window
 
