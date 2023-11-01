a23dfd72740832f19c5de42cce1d4c4465e50bd2
Bryton Lacquement <hello@brytonlacquement.com>
2023-11-01 14:21:34 +0100

    nvim: add lsp supporte (with mason)

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -8,16 +8,20 @@ return {
         'nvim-telescope/telescope.nvim', tag = '0.1.4',
         dependencies = { 'nvim-lua/plenary.nvim' },
         keys = {
-            { "<leader>f:", "<cmd>Telescope command_history<cr>", desc = "Command History" },
-            { "<leader>f<CR>", function() require("telescope.builtin").resume() end, desc = "Resume Previous Search" },
-            { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Find Buffers" },
-            { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-            { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
-            { "<leader>fh", "<cmd>Telescope oldfiles<cr>", desc = "Find History" },
-            { "<leader>fm", "<cmd>Telescope man_pages<cr>", desc = "Find Man Pages" },
+            { "<leader>f:", "<cmd>Telescope command_history<cr>", desc = "Command history" },
+            { "<leader>f<CR>", function() require("telescope.builtin").resume() end, desc = "Resume previous search" },
+            { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Find buffers" },
+            { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
+            { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
+            { "<leader>fh", "<cmd>Telescope oldfiles<cr>", desc = "Find history" },
+            { "<leader>fm", "<cmd>Telescope man_pages<cr>", desc = "Find man pages" },
 
-            { "<leader>gc", "<cmd>Telescope git_commits<cr>", desc = "Git Commits" },
-            { "<leader>gs", "<cmd>Telescope git_status<cr>", desc = "Git Status" },
+            { "<leader>gc", "<cmd>Telescope git_branches<cr>", desc = "Git branches" },
+            { "<leader>gc", "<cmd>Telescope git_commits<cr>", desc = "Git commits" },
+            { "<leader>gs", "<cmd>Telescope git_status<cr>", desc = "Git status" },
+
+            { "gd", "<cmd>Telescope lsp_definitions<cr>", desc = "Go to definition(s)" },
+            { "gr", "<cmd>Telescope lsp_references<cr>", desc = "Go to references" },
         },
     },
 
@@ -49,41 +53,6 @@ return {
         end,
     },
 
-    {
-        "folke/noice.nvim",
-        event = "VeryLazy",
-        opts = {
-            presets = {
-                command_palette = true, -- position the cmdline and popupmenu together
-                long_message_to_split = true, -- long messages will be sent to a split
-                inc_rename = false, -- enables an input dialog for inc-rename.nvim
-                lsp_doc_border = false, -- add a border to hover docs and signature help
-            },
-            cmdline = {
-                format = {
-                    command = { pattern = "^:", icon = ":", lang = "vim" },
-                    search_down = { kind = "search", pattern = "^/", icon = "/", lang = "regex" },
-                    search_up = { kind = "search", pattern = "^%?", icon = "?", lang = "regex" },
-                    bash = { pattern = "^:%s*!", icon = ":!", lang = "bash" },
-                    help = { pattern = "^:%s*he?l?p?%s+", icon = ":help" },
-                    input = {}, -- Used by input()
-                    -- lua = false, -- to disable a format, set to `false`
-                },
-            },
-            messages = {
-                view_search = false,
-            },
-        },
-        dependencies = {
-            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-            "MunifTanjim/nui.nvim",
-            -- OPTIONAL:
-            --   `nvim-notify` is only needed, if you want to use the notification view.
-            --   If not available, we use `mini` as the fallback
-            "rcarriga/nvim-notify",
-        },
-    },
-
     { 
         'echasnovski/mini.map', 
         config = function()
@@ -104,7 +73,7 @@ return {
         opts = {
             indent = {
                 char = "┆",
-                tab_char = "┆",
+                -- tab_char = "┆",
             },
             scope = { enabled = false },
         },
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,7 +1,50 @@
 return {
     {
-        "",
-        config = function()
-        end,
+        'williamboman/mason.nvim',
+        lazy = false,
+        config = true,
+    },
+
+    {
+        'williamboman/mason-lspconfig.nvim',
+        lazy = false,
+        opts = {
+            ensure_installed = {
+                'ansiblels',
+                'bashls',
+                'cssls',
+                'dockerls',
+                'docker_compose_language_service',
+                'html',
+                'jsonls',
+                'tsserver',  -- js
+                'marksman',  -- markdown
+                'intelephense',  -- php
+                'pyright',  -- python
+                'sqlls',
+                'taplo',  -- toml
+                'tailwindcss',
+                'lemminx',  -- xml
+                'yamlls',
+            },
+        },
+        config = function(_, opts)
+            require("mason-lspconfig").setup(opts)
+            require("mason-lspconfig").setup_handlers {
+                function (server_name)
+                    require("lspconfig")[server_name].setup {}
+                end,
+            }
+        end
+    },
+
+    {
+        'neovim/nvim-lspconfig',
+        lazy = false,
+        keys = {
+            { 'K', '<cmd>lua vim.lsp.buf.hover()<cr>' },
+            { 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', desc = "Go to definition" },
+            -- { '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>' },
+        },
     },
 }
